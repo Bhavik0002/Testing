bb70ad2a34b949a57ffe6ddcd71b5975cfbf3c85
Bhavik0002 <>
2023-01-30 11:09:32 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let a = [1,2,3,4,5,6];
+let arr2 = ['bhavik','vivek','amit'];
+let index = 3;
+// console.log(a.at(index));
+
+// console.log(a.concat(arr2));
+
+// console.log(a.fill(3));
+// console.log(a.fill(2,1));
+console.log(a.fill(9,3,10));
